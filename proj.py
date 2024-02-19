5c14837d433d8aa4037a92fb7a0d2eedf5e7a171
Jonathan Wulf <j.wulf@live.com>
2024-02-19 16:42:40 -0600

    begin groundwork w/ tensors

--- proj.py
+++ proj.py
@@ -1,3 +1,15 @@
 import tensorflow as tf
+print()
+print()
 print(tf.version)
 print("Hello World")
+
+
+rank1_tensor = tf.Variable(["word", "another word"], tf.string)
+rank2_tensor = tf.Variable([["word", "yes"],["another word", "no"]], tf.string)
+print('rank1_tensor = ')
+print(tf.rank(rank1_tensor))
+print('rank2_tensor = ')
+print(tf.rank(rank2_tensor))
+
+
